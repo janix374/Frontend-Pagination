10574d54afb89a7ab6ba89a2635a48330327ce1d
janix374 <janko.kostic@yahoo.com>
2020-10-07 03:33:39 +0200

    'styling'

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ListGroup, Row, Col } from 'react-bootstrap';
 
-const Posts = ({ posts,isLoading  }) => {
+const Posts = ({ posts, isLoading }) => {
 
     return (
         <Row className='my-4 '>
@@ -10,7 +10,7 @@ const Posts = ({ posts,isLoading  }) => {
                 ):(
                     <ListGroup>
                         {posts.map((post) => {
-                            return (<ListGroup.Item key={post.id}><span className='text-danger'>{post.id}. </span>
+                            return (<ListGroup.Item key={post.id}><span className='text-primary'>{post.id}. </span>
                                 <span>{post.title}</span></ListGroup.Item>)
                         })}
                     </ListGroup>
