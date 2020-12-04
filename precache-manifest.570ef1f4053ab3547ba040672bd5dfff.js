4f12fb4ad7ce607fa695b402833f8f5cd3e0e8f5
janix374 <janko.kostic@yahoo.com>
2020-12-04 14:39:42 +0100

    Updates

---
+++ precache-manifest.570ef1f4053ab3547ba040672bd5dfff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5c23d335a4d63e6db0fbf46d845d3c6",
+    "url": "/janix374/Frontend-Pagination/index.html"
+  },
+  {
+    "revision": "65d0103efb47dd4fdf9a",
+    "url": "/janix374/Frontend-Pagination/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f62349d7ab794a8fda99",
+    "url": "/janix374/Frontend-Pagination/static/css/main.b3fba224.chunk.css"
+  },
+  {
+    "revision": "65d0103efb47dd4fdf9a",
+    "url": "/janix374/Frontend-Pagination/static/js/2.e105b2be.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/janix374/Frontend-Pagination/static/js/2.e105b2be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f62349d7ab794a8fda99",
+    "url": "/janix374/Frontend-Pagination/static/js/main.e6e9ccfb.chunk.js"
+  },
+  {
+    "revision": "7a698a2da208ed280b62",
+    "url": "/janix374/Frontend-Pagination/static/js/runtime-main.d1ced246.js"
+  }
+]);
