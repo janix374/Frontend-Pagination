170b3d70cb37e5b84a36fb70f889336be4860c66
janix374 <janko.kostic@yahoo.com>
2020-12-19 01:02:51 +0100

    Updates

--- precache-manifest.744731d3c51968897c7937ad021b8e74.js
+++ precache-manifest.744731d3c51968897c7937ad021b8e74.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4ac4e83fba012afe93fd9c2bc048f925",
+    "revision": "fa6bfb27dfc9c63453dad59e27b0501f",
     "url": "/React-Frontend-Pagination/index.html"
   },
   {
-    "revision": "65d0103efb47dd4fdf9a",
+    "revision": "ca69a8b6e3ee71c2a000",
     "url": "/React-Frontend-Pagination/static/css/2.11829350.chunk.css"
   },
   {
-    "revision": "8e0f70c7f5ee8724c728",
+    "revision": "fb7a25ec4fc27ce573d0",
     "url": "/React-Frontend-Pagination/static/css/main.b3fba224.chunk.css"
   },
   {
-    "revision": "65d0103efb47dd4fdf9a",
-    "url": "/React-Frontend-Pagination/static/js/2.e105b2be.chunk.js"
+    "revision": "ca69a8b6e3ee71c2a000",
+    "url": "/React-Frontend-Pagination/static/js/2.6a395f0e.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/React-Frontend-Pagination/static/js/2.e105b2be.chunk.js.LICENSE.txt"
+    "url": "/React-Frontend-Pagination/static/js/2.6a395f0e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8e0f70c7f5ee8724c728",
-    "url": "/React-Frontend-Pagination/static/js/main.d1dc6da7.chunk.js"
+    "revision": "fb7a25ec4fc27ce573d0",
+    "url": "/React-Frontend-Pagination/static/js/main.f3cce086.chunk.js"
   },
   {
     "revision": "bd436e7646617b19cae2",
